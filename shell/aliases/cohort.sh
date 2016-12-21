e588353c3fb4fb7a6d66590098d20ba68c1221be
Gabriel Gizotti <gabriel@gizotti.com>
2016-12-21 10:26:27 +1000

    add mirror and fakes3 aliases

--- shell/aliases/cohort.sh
+++ shell/aliases/cohort.sh
@@ -5,3 +5,6 @@ alias cpay='cd ~/Dev/cohortpay'
 alias csim='cd ~/Dev/cohortsim'
 alias ctrans='cd ~/Dev/cohortpay-transact'
 alias oshc='cd ~/Dev/oshcaustralia'
+
+alias runmirror='cd ~/Dev/mirror && source .env && thin -R config.ru -p 9292 start'
+alias runfakes3='cd ~/Dev/fake-s3 && bin/fakes3 server -r `pwd`/tmp -p 10001 -h s3.dev'
