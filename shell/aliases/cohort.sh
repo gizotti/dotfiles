f5ee3d8652cc573a27549307ba957170f5d7663e
Gabriel Gizotti <gabriel@gizotti.com>
2017-02-14 12:45:56 +1000

    add more cohort aliases

--- shell/aliases/cohort.sh
+++ shell/aliases/cohort.sh
@@ -8,3 +8,6 @@ alias oshc='cd ~/Dev/oshcaustralia'
 
 alias runmirror='cd ~/Dev/mirror && source .env && thin -R config.ru -p 9292 start'
 alias runfakes3='cd ~/Dev/fake-s3 && bin/fakes3 server -r `pwd`/tmp -p 10001 -h s3.dev'
+
+alias linkcc='bundle config local.cohort_core ~/Dev/cohort-core/'
+alias unlinkcc='bundle config --delete local.cohort_core'
