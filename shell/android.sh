2863fa15d704046c57eb212aff018ffcab9b2208
Gabriel Gizotti <gabriel@gizotti.com>
2017-11-12 22:30:06 +1000

    define ANDROID_SDK_ROOT var

--- shell/android.sh
+++ shell/android.sh
@@ -1,3 +1,4 @@
 export ANDROID_HOME=${HOME}/Library/Android/sdk
+export ANDROID_SDK_ROOT=${HOME}/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
