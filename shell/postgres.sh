68960c55093bb86f9b355a8809b64641c21c95be
Gabriel Gizotti <gabriel@gizotti.com>
2019-05-24 13:30:20 +1000

    add more pg functions

--- shell/postgres.sh
+++ shell/postgres.sh
@@ -1,11 +1,23 @@
 pgswitch() {
-  logfile="/usr/local/var/log/postgres@$2.log"
+  pgstop $1
+  pgstart $2
+}
+
+pgstart() {
+  echo "Starting PG $1"
+
+  logfile="/usr/local/var/log/postgres@$1.log"
 
   if [ ! -f $logfile ]
   then
     touch $logfile
   fi
 
+  ~/.asdf/installs/postgres/$1/bin/pg_ctl -D ~/.asdf/installs/postgres/$1/data -l $logfile start
+}
+
+pgstop() {
+  echo "Stopping PG $1"
+
   ~/.asdf/installs/postgres/$1/bin/pg_ctl -D ~/.asdf/installs/postgres/$1/data stop
-  ~/.asdf/installs/postgres/$2/bin/pg_ctl -D ~/.asdf/installs/postgres/$2/data -l $logfile start
 }
