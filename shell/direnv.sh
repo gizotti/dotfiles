f72740c30655ef4b4f8cf4df5551e6d94b0c2c1f
Gabriel Gizotti <gabriel@gizotti.com>
2025-02-13 15:41:45 +1000

    source nix-direnv

--- shell/direnv.sh
+++ shell/direnv.sh
@@ -1,1 +1,8 @@
+# nix-direnv
+file="$HOME/.nix-profile/share/nix-direnv/direnvrc" 
+if [ -f "$file" ]
+then
+  source $file
+fi
+
 eval "$(direnv hook zsh)"
--- shell/nix.sh
+++ shell/nix.sh
@@ -1,5 +1,5 @@
 # Nix
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 # End Nix
